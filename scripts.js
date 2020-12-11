e88169107df0f3dc71cb0c4723a84d13d92d63c5
kitsune777 <watermonkey777gmail.com>
2020-12-11 13:57:09 -0800

    added currentTarget event to getAnimal()

--- scripts.js
+++ scripts.js
@@ -1,12 +1,4 @@
-// const animalSeal = document.getElementById("seal");
-// const animalFox = document.getElementById("fox");
-// const animalSeaLion = document.getElementById("sea_lion");
-// const animalWolf = document.getElementById("wolf");
-// const animalLemur = document.getElementById("lemur");
-// const animalShark1 = document.getElementById("shark1");
-// const animalShark2 = document.getElementById("shark2");
-// const animalTiger = document.getElementById("tiger");
-// const animalLeopard = document.getElementById("leopard");
+const modalInfo = document.getElementById("modalInfo");
 
 function loginFunc() {
   console.log("login event listener works");
@@ -14,5 +6,6 @@ function loginFunc() {
 
 function getAnimal(event) {
   event.preventDefault();
-  console.log("clicked here");
+  console.log("clicked on " + event.currentTarget.title);
+  document.getElementById("modalInfo");
 }
