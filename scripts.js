0dc506bf451477b350a22979b42c9faa87f96cec
kitsune777 <watermonkey777gmail.com>
2020-12-11 13:39:20 -0800

    clickable map/nav

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// const animalSeal = document.getElementById("seal");
+// const animalFox = document.getElementById("fox");
+// const animalSeaLion = document.getElementById("sea_lion");
+// const animalWolf = document.getElementById("wolf");
+// const animalLemur = document.getElementById("lemur");
+// const animalShark1 = document.getElementById("shark1");
+// const animalShark2 = document.getElementById("shark2");
+// const animalTiger = document.getElementById("tiger");
+// const animalLeopard = document.getElementById("leopard");
+
+function loginFunc() {
+  console.log("login event listener works");
+}
+
+function getAnimal(event) {
+  event.preventDefault();
+  console.log("clicked here");
+}
