ec7bc277dc30ef08ccfd9ddd9c8c9a1fc0ca168f
kitsune777 <watermonkey777gmail.com>
2020-12-14 11:09:47 -0800

    added videos for each animal on page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,33 @@
-const modalInfo = document.getElementById("modalInfo");
-
 function loginFunc() {
   console.log("login event listener works");
 }
 
+//function to get info card for each animal.
 function getAnimal(event) {
-  event.preventDefault();
+  event.preventDefault(event);
   console.log("clicked on " + event.currentTarget.title);
-  document.getElementById("modalInfo");
+  popCard();
+}
+//Jason's Info card code......
+const cardWrapper = document.querySelector(".card_wrapper");
+const cardContainer = document.querySelector(".card_container");
+const closeCardBtn = document.querySelector(".close_btn");
+
+function popCard() {
+  cardWrapper.style.display = "block";
+}
+
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target == cardContainer) {
+    cardWrapper.style.display = "none";
+  }
+  if (e.target == closeCardBtn) {
+    cardWrapper.style.display = "none";
+  }
+});
+
+vids = document.getElementById("video_container");
+function getVideos() {
+  fetch("/animal:videos");
 }
