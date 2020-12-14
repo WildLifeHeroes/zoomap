3e5b4aed311cb753a720bdcf7bcf343ad18ebf9a
kitsune777 <watermonkey777gmail.com>
2020-12-14 12:55:38 -0800

    videos html/css/js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,38 +4,15 @@
 const cardWrapper = document.querySelector(".card_wrapper");
 const cardContainer = document.querySelector(".card_container");
 const closeCardBtn = document.querySelector(".close_btn");
-const outterLoginContainer = document.querySelector("#outter_login_container");
-
-// used to close animal card if user click area outside of animal card or close button.
-window.addEventListener("click", (e) => {
-  closeEvent(e);
-});
-
-window.addEventListener('keypress', (e) => {
-  if (e.code = "Enter") {
-    closeEvent(e);
-  }
-})
-
-function closeEvent(e) {
-  if (e.target == cardContainer || e.target == closeCardBtn) {
-    cardWrapper.style.display = "none";
-    clearImg();
-  }
-
-  if (e.target == outterLoginContainer || e.target == loginContainer) {
-    loginContainer.style.display = "none";
-  }
-}
 
 function getAnimal(e) {
   e.preventDefault(); // prevent default behaviors
-  clearImg();
   infoRequest(e); // fetch data, build card, pop display the card.
 }
 
 function infoRequest(e) {
   let animalTitle = e.target.title;
+  console.log(animalTitle);
   urlBuilder(animalTitle); // get current animal
 }
 
@@ -54,10 +31,7 @@ function callingAPI(url) {
     .then(function (response) {
       // handle success
       const animalBack = response.data;
-<<<<<<< HEAD
       console.log(animalBack);
-=======
->>>>>>> 9a76ff3a5db520d20e853570ea400b9ceffaed8a
 
       setCard(
         // send data to make card relevent to the animal
@@ -74,7 +48,7 @@ function callingAPI(url) {
 
 //initiate setting animal card.
 function setCard(img, name, info) {
-  const card = getCard_Wrapper();
+  const card = makeCard_Wrapper();
   const classname = "." + `${card.className}`;
   const target = document.querySelector(classname);
 
@@ -87,12 +61,16 @@ function setCard(img, name, info) {
 
 // card setter.
 function setCard_Images(img) {
+  // let im = document.querySelector(".AnimalImg");
+  // let u = `"${img}"`;
+  // im.style.background = `url(${u})`;
   if (img !== null) {
     let imgContainer = document.querySelector(".AnimalImg");
     let theImg = document.createElement("img");
-    imgContainer.style.background = "none";
     theImg.setAttribute("id", "animal_thumnail");
     theImg.src = img;
+    // theImg.style.width = "180px";
+    // theImg.style.height = "180px";
     imgContainer.appendChild(theImg);
   }
 }
@@ -110,8 +88,19 @@ function setCard_descriptions(info) {
 // show card upon click event triggered.
 function popCard(card) {
   card.style.display = "block";
-  document.getElementById("prev").focus();
 }
+
+// used to close animal card if user click area outside of animal card or close button.
+window.addEventListener("click", (e) => {
+  if (e.target == cardContainer) {
+    cardWrapper.style.display = "none";
+    clearImg();
+  }
+  if (e.target == closeCardBtn) {
+    cardWrapper.style.display = "none";
+    clearImg();
+  }
+});
 
 function clearImg() {
   if (document.querySelector("#animal_thumnail") !== null) {
@@ -120,8 +109,10 @@ function clearImg() {
   }
 }
 
-function getCard_Wrapper() {
-  const card_wrapper = document.querySelector(".card_wrapper");
+// parent element generator.
+function makeCard_Wrapper() {
+  const card_wrapper = document.createElement("div");
+  card_wrapper.setAttribute("class", "card_wrapper");
   return card_wrapper;
 }
 
@@ -130,7 +121,6 @@ function getCard_Wrapper() {
  *********************************/
 
 /*********************************
-<<<<<<< HEAD
  * Animal Videos
  *********************************/
 const animalVids = document.getElementById("video_container");
@@ -191,16 +181,3 @@ function getVideosApi(urlPath) {
 /*********************************
  * Animal Videos end
  *********************************/
-=======
- * Login and register
- *********************************/
-const loginContainer = document.querySelector("#login_container");
-
-function loginFunc() {
-  loginContainer.style.display = "block";
-}
-
-/*********************************
- * Login and register end
- *********************************/
->>>>>>> 9a76ff3a5db520d20e853570ea400b9ceffaed8a
