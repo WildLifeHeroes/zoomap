42c8da329dd25c90bea985917b8df51f2b2d90bb
dustinmorris1 <dustinnuc@gmail.com>
2020-12-15 15:29:04 -0800

    fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 
-app.use(morgan('dev')); // dev only
 app.use(cors()); //TODO: See about restricting cors
 app.use('/', express.static(PUBLIC_DIR));
 
